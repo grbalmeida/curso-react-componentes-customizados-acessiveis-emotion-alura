aa966318c9595e2775aaaf30cebf6d689e1957c2
grbalmeida <g.r.almeida@live.com>
2023-10-18 20:14:32 -0300

    Criando o provedor do tema

--- freelando/src/App.js
+++ freelando/src/App.js
@@ -1,15 +1,16 @@
 import { Card } from './componentes/Card/Card';
 import { Estilos } from './componentes/EstilosGlobais/Estilos';
+import { ProvedorTema } from './componentes/ProvedorTema/ProvedorTema';
 
 function App() {
   return (
     <>
-      <div>
+      <ProvedorTema>
         <Estilos />
         <Card>
           <h1>Freelando</h1>
         </Card>
-      </div>
+      </ProvedorTema>
     </>
   );
 }
