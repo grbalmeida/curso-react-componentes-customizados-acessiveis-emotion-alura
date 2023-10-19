f1c3468d3a565c8f9be0600bbbbd116e0162dac3
grbalmeida <g.r.almeida@live.com>
2023-10-19 11:37:05 -0300

    Entendendo o sistema de Grid

--- freelando/src/App.js
+++ freelando/src/App.js
@@ -1,3 +1,5 @@
+import { Container } from 'react-grid-system';
+
 import { CampoTexto } from './componentes/CampoTexto/CampoTexto';
 import { Card } from './componentes/Card/Card';
 import { Estilos } from './componentes/EstilosGlobais/Estilos';
@@ -9,22 +11,24 @@ function App() {
     <>
       <ProvedorTema>
         <Estilos />
-        <Card>
-          <Tipografia
-            variante="h1"
-            componente="h1"
-          >
-            Freelando
-          </Tipografia>
-          <Tipografia
-            variante="body"
-            componente="body"
-          >
-            Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers.
-            Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
-          </Tipografia>
-          <CampoTexto titulo="Nome Completo" />
-        </Card>
+        <Container>
+          <Card>
+            <Tipografia
+              variante="h1"
+              componente="h1"
+            >
+              Freelando
+            </Tipografia>
+            <Tipografia
+              variante="body"
+              componente="body"
+            >
+              Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers.
+              Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
+            </Tipografia>
+            <CampoTexto titulo="Nome Completo" />
+          </Card>
+        </Container>
       </ProvedorTema>
     </>
   );
