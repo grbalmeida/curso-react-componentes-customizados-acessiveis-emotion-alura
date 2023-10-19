7fced00239689debc9361e0f90632038a665f5ad
grbalmeida <g.r.almeida@live.com>
2023-10-19 14:35:47 -0300

    Finalizando a estrutura da página

--- freelando/src/App.js
+++ freelando/src/App.js
@@ -1,20 +1,41 @@
 import { Col, Container, Row } from 'react-grid-system';
 
+import { Cabecalho } from './componentes/Cabecalho/Cabecalho';
+import { FreelandoLogo } from './componentes/Icones/FreelandoLogo';
+import { Link } from './componentes/Link/Link';
 import { CampoTexto } from './componentes/CampoTexto/CampoTexto';
 import { Card } from './componentes/Card/Card';
 import { Estilos } from './componentes/EstilosGlobais/Estilos';
 import { ProvedorTema } from './componentes/ProvedorTema/ProvedorTema';
 import { Tipografia } from './componentes/Tipografia/Tipografia';
 import { Botao } from './componentes/Botao/Botao';
+import { Rodape } from './componentes/Rodape/Rodape';
+import { ListaInline } from './componentes/Lista/ListaInline';
+import { ItemListaInline } from './componentes/Lista/ItemListaInline';
+import { IconeWhatsApp } from './componentes/Icones/IconeWhatsApp';
+import { IconeTwitch } from './componentes/Icones/IconeTwitch';
+import { IconeInstagram } from './componentes/Icones/IconeInstagram';
+import { IconeTwitter } from './componentes/Icones/IconeTwitter';
 
 function App() {
   return (
     <>
       <ProvedorTema>
         <Estilos />
-        <Container style={{ margin: '80px 0' }}>
-          <Row justify="center">
-            <Col lg={6} md={8} sm={12}>
+        <Cabecalho>
+          <Container>
+            <Row align="center">
+              <Col>
+                <FreelandoLogo />
+              </Col>
+              <Col style={{ textAlign: 'right' }}>
+                <Link>Login</Link>
+              </Col>
+            </Row>
+          </Container>
+        </Cabecalho>
+        <Row justify="center">
+          <Col xxx={6} xxl={6} xl={6} lg={6} md={8} sm={12} style={{ margin: '80px 0' }}>
               <Card>
                 <Tipografia
                   variante="h1"
@@ -68,7 +89,41 @@ function App() {
               </Card>
             </Col>
           </Row>
-        </Container>
+          <Rodape>
+            <Container>
+              <Row align="center">
+                <Col>
+                  <FreelandoLogo height={40} width={176} />
+                  <Tipografia variante="legenda" componente="legenda">Desenvolvido por Alura. Projeto fictício sem fins comerciais.</Tipografia>
+                </Col>
+                <Col style={{ textAlign: 'right' }}>
+                  <Tipografia variante="legenda" componente="legenda">Acesse nossas redes:</Tipografia>
+                  <ListaInline>
+                    <ItemListaInline>
+                      <a href="/" aria-label="Link para o WhatsApp">
+                        <IconeWhatsApp />
+                      </a>
+                    </ItemListaInline>
+                    <ItemListaInline>
+                      <a href="/" aria-label="Link para a Twitch">
+                        <IconeTwitch />
+                      </a>
+                    </ItemListaInline>
+                    <ItemListaInline>
+                      <a href="/" aria-label="Link para o Instagram">
+                        <IconeInstagram />
+                      </a>
+                    </ItemListaInline>
+                    <ItemListaInline>
+                      <a href="/" aria-label="Link para o Twitter">
+                        <IconeTwitter />
+                      </a>
+                    </ItemListaInline>
+                  </ListaInline>
+                </Col>
+              </Row>
+            </Container>
+          </Rodape>
       </ProvedorTema>
     </>
   );
