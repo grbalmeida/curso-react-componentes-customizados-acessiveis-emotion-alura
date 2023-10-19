f1c3bab3236bff4a604abd5e42e33a86005443a5
grbalmeida <g.r.almeida@live.com>
2023-10-19 07:56:22 -0300

    Idealizando a tipografia

--- freelando/src/App.js
+++ freelando/src/App.js
@@ -1,6 +1,7 @@
 import { Card } from './componentes/Card/Card';
 import { Estilos } from './componentes/EstilosGlobais/Estilos';
 import { ProvedorTema } from './componentes/ProvedorTema/ProvedorTema';
+import { Tipografia } from './componentes/Tipografia/Tipografia';
 
 function App() {
   return (
@@ -8,7 +9,12 @@ function App() {
       <ProvedorTema>
         <Estilos />
         <Card>
-          <h1>Freelando</h1>
+          <Tipografia
+            variante="h1"
+            componente="h1"
+          >
+            Freelando
+          </Tipografia>
         </Card>
       </ProvedorTema>
     </>
