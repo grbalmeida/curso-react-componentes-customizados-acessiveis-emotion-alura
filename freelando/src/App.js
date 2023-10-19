87ea2a548f905051a9809c92a27f7b0c7f4c2c5f
grbalmeida <g.r.almeida@live.com>
2023-10-19 11:59:11 -0300

    Criar botão e variante

--- freelando/src/App.js
+++ freelando/src/App.js
@@ -5,6 +5,7 @@ import { Card } from './componentes/Card/Card';
 import { Estilos } from './componentes/EstilosGlobais/Estilos';
 import { ProvedorTema } from './componentes/ProvedorTema/ProvedorTema';
 import { Tipografia } from './componentes/Tipografia/Tipografia';
+import { Botao } from './componentes/Botao/Botao';
 
 function App() {
   return (
@@ -29,6 +30,8 @@ function App() {
                   Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
                 </Tipografia>
                 <CampoTexto titulo="Nome Completo" />
+                <Botao variante="secundaria">Botão secundário</Botao>
+                <Botao variante="primaria">Botão primário</Botao>
               </Card>
             </Col>
           </Row>
