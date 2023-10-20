1fbfe39dbf7a5b6328396f2984b4b5020ce8575d
grbalmeida <g.r.almeida@live.com>
2023-10-20 18:28:04 -0300

    Estilizando a lista suspensa

--- freelando/src/componentes/ListaSuspensa/ListaSuspensa.jsx
+++ freelando/src/componentes/ListaSuspensa/ListaSuspensa.jsx
@@ -1,6 +1,37 @@
 import styled from '@emotion/styled';
 import { useState } from 'react';
 
+const ItemListaSuspensaEstilizado = styled.li`
+    padding: ${props => props.theme.espacamentos.xs} 0;
+    text-align: center;
+    border-bottom: 1px solid ${props => props.theme.cores.neutras.c};
+    cursor: pointer;
+
+    &:last-child {
+        border: none;
+    }
+
+    &:hover {
+        color: ${props => props.theme.cores.focus};
+    }
+`;
+
+const ListaSuspensaEstilizada = styled.ul`
+    position: absolute;
+    top: 100%;
+    left: 0;
+    right: 0;
+    background-color: ${props => props.theme.cores.branco};
+    z-index: 1;
+    border: 1px solid ${props => props.theme.cores.neutras.a};
+    border-bottom-left-radius: 18px;
+    border-bottom-right-radius: 18px;
+    border-top: none;
+    margin: 0;
+    padding: 0 ${props => props.theme.espacamentos.m};
+    list-style: none;
+`;
+
 const LabelEstilizada = styled.label`
     display: block;
     width: 100%;
@@ -54,11 +85,11 @@ export const ListaSuspensa = ({ titulo, opcoes }) => {
                 </div>
             </BotaoEstilizado>
             {estaAberta && 
-                <ul>
+                <ListaSuspensaEstilizada>
                     {opcoes.map(opcao => (
-                        <li key={opcao.value}>{opcao.text}</li>
+                        <ItemListaSuspensaEstilizado key={opcao.value}>{opcao.text}</ItemListaSuspensaEstilizado>
                     ))}
-                </ul>
+                </ListaSuspensaEstilizada>
             }
         </LabelEstilizada>
     )
